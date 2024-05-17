627c185b38b31be64bbc52819f6d46b3e7b29009
Wojciech <kowalczyk.wojtek168@gmail.com>
2024-05-17 19:41:45 +0200

    zad4 done

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import company.abstracts.Employee;
 import company.models.Manager;
@@ -10,23 +12,74 @@ public class Main {
         Worker worker1 = new Worker("Alice", 3000, 1, "2022-01-15", "Worker");
         Worker worker2 = new Worker("Bob", 3200, 1, "2022-02-20", "Worker");
         Worker worker3 = new Worker("Charlie", 3100, 3, "2022-03-10", "Worker");
+        Worker worker4 = new Worker("Diana", 3050, 1, "2022-04-05", "Worker");
 
-        Manager manager = new Manager("Edward", 5000, 5, "2021-05-01", "Manager");
+        Manager manager1 = new Manager("Edward", 5000, 5, "2021-05-01", "Manager");
+        Manager manager2 = new Manager("Fiona", 5200, 3, "2021-06-01", "Manager");
 
         List<Employee> employees = new ArrayList<>();
         employees.add(worker1);
         employees.add(worker2);
         employees.add(worker3);
-        employees.add(manager);
+        employees.add(worker4);
+        employees.add(manager1);
+        employees.add(manager2);
 
+        double totalSalary = calculateTotalSalary(employees);
+        double totalManagerSalary = calculateTotalManagerSalary(employees);
+        double totalWorkerSalary = calculateTotalWorkerSalary(employees);
+
+        System.out.println("Total salary of all employees: " + totalSalary);
+        System.out.println("Total salary of all managers: " + totalManagerSalary);
+        System.out.println("Total salary of all workers: " + totalWorkerSalary);
+
+        displayDuplicateEmployees(employees);
+    }
+
+    private static double calculateTotalSalary(List<Employee> employees) {
+        double totalSalary = 0;
         for (Employee employee : employees) {
-            System.out.println(employee.getName() + " has code: " + employee.hashCode());
+            totalSalary += employee.getSalary();
         }
+        return totalSalary;
+    }
 
-        System.out.println("\nEquality checks:");
+    private static double calculateTotalManagerSalary(List<Employee> employees) {
+        double totalManagerSalary = 0;
         for (Employee employee : employees) {
-            boolean isEqual = worker1.equals(employee);
-            System.out.println("Worker1 equals " + employee.getName() + ": " + isEqual);
+            if (employee instanceof Manager) {
+                totalManagerSalary += employee.getSalary();
+            }
+        }
+        return totalManagerSalary;
+    }
+
+    private static double calculateTotalWorkerSalary(List<Employee> employees) {
+        double totalWorkerSalary = 0;
+        for (Employee employee : employees) {
+            if (employee instanceof Worker) {
+                totalWorkerSalary += employee.getSalary();
+            }
+        }
+        return totalWorkerSalary;
+    }
+
+    private static void displayDuplicateEmployees(List<Employee> employees) {
+        System.out.println("\nEmployees with duplicate IDs:");
+        Map<Integer, List<Employee>> employeeMap = new HashMap<>();
+    
+        for (Employee employee : employees) {
+            employeeMap.computeIfAbsent(employee.hashCode(), k -> new ArrayList<>()).add(employee);
+        }
+    
+        for (List<Employee> employeeList : employeeMap.values()) {
+            if (employeeList.size() > 1) {
+                System.out.print(employeeList.get(0).getName() + " (ID: " + employeeList.get(0).hashCode() + ")");
+                for (int i = 1; i < employeeList.size(); i++) {
+                    System.out.print(", " + employeeList.get(i).getName() + " (ID: " + employeeList.get(i).hashCode() + ")");
+                }
+                System.out.println(" have the same ID.");
+            }
         }
     }
 }
